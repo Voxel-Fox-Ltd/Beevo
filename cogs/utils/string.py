809b3ff2feb331a40c595f59ccce248c4948face
Kae Bartlett <kae@voxelfox.co.uk>
2021-08-22 21:06:42 +0100

    Add a join formatter I don't think I need it but I wrote it

--- cogs/utils/string.py
+++ cogs/utils/string.py
@@ -26,5 +26,25 @@ class PronounFormatter(string.Formatter):
         return super().format_field(value, spec)
 
 
-class Formatter(PluralFormatter, PronounFormatter):
+class JoinFormatter(string.Formatter):
+
+    def format_field(self, value: typing.Any, spec: str) -> str:
+        spec_split = spec.split(",", 1)
+        if spec_split[0] == "join":
+            try:
+                joiner = spec_split[1]
+            except IndexError:
+                joiner = ", "
+            return joiner.join(value)
+        elif spec_split[0] == "humanjoin":
+            if len(value) == 1:
+                return value[0]
+            elif len(value) == 2:
+                return " and ".join(value)
+            else:
+                return ", ".join(value[:-1]) + f", and {value[-1]}"
+        return super().format_field(value, spec)
+
+
+class Formatter(PluralFormatter, PronounFormatter, JoinFormatter):
     pass

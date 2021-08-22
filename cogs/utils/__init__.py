e4d9cc03682a1d26de62206f8724754c7ff776d4
Kae Bartlett <kae@voxelfox.co.uk>
2021-08-22 21:12:45 +0100

    Remove old hive cell formatting for now It might come back later

--- cogs/utils/__init__.py
+++ cogs/utils/__init__.py
@@ -1,7 +1,6 @@
 from .bee import Bee, BeeType, Nobility
 from .hive import Hive
 from .string import Formatter as _Formatter
-# from .hive_cell_emoji import HiveCellEmoji
 
 _formatter = _Formatter()
 format = _formatter.format

26252226fe0b0424eafa2f96fbf844efb81c97ae
Kae Bartlett <kae@voxelfox.co.uk>
2021-08-23 04:25:37 +0100

    Set bee breeding to use select menus

--- cogs/bee_commands.py
+++ cogs/bee_commands.py
@@ -1,4 +1,5 @@
 import collections
+import asyncio
 
 import voxelbotutils as vbu
 import discord
@@ -156,24 +157,83 @@ class BeeCommands(vbu.Cog):
         )
 
     @bee.command(name="breed")
+    @vbu.defer()
     @commands.guild_only()
-    async def bee_breed(self, ctx: vbu.Context, princess: utils.Bee, drone: utils.Bee):
+    async def bee_breed(self, ctx: vbu.Context):
         """
         Breed one of your princesses and drones into a queen.
         """
 
-        # Defer our response because we love database lag
-        await ctx.defer()
+        # Work out what bees they have available to breed
+        async with self.bot.database() as db:
+            bees = await utils.Bee.fetch_bees_by_user(db, ctx.guild.id, ctx.author.id)
+        princesses = {}
+        drones = {}
+        for bee in bees:
+            if bee.hive_id:
+                continue
+            if bee.nobility == utils.Nobility.PRINCESS:
+                princesses[bee.id] = bee
+            elif bee.nobility == utils.Nobility.DRONE:
+                drones[bee.id] = bee
+
+        # See if they have any available
+        if not princesses or not drones:
+            return await ctx.send("You don't have an available princess and drone to breed :<", wait=False)
+
+        # Make a check
+        def get_message_check(message):
+            def check(payload):
+                if payload.message.id != message.id:
+                    return False
+                if payload.user.id != ctx.author.id:
+                    self.bot.loop.create_task(payload.send(f"Only {ctx.author.mention} can interact with this select menu :<", wait=False))
+                    return False
+                return True
+            return check
+
+        # Ask which princess they want to breed
+        components = vbu.MessageComponents(vbu.ActionRow(vbu.SelectMenu(
+            custom_id="BREED BEE_SELECTION",
+            options=[
+                vbu.SelectOption(label=bee.name, value=bee.id, description=bee.type_display)
+                for bee in princesses.values()
+            ],
+            placeholder="Which princess would you like to breed?"
+        )))
+        princess_message = await ctx.send("Which of your princesses would you like to breed?", components=components)
+        try:
+            payload = await self.bot.wait_for("component_interaction", check=get_message_check(princess_message), timeout=60)
+        except asyncio.TimeoutError:
+            return await ctx.send("I timed out waiting for you to say which princess you want to breed :c", wait=False)
+        princess = princesses[payload.values[0]]
+
+        # Ask which drone they want to breed
+        components = vbu.MessageComponents(vbu.ActionRow(vbu.SelectMenu(
+            custom_id="BREED BEE_SELECTION",
+            options=[
+                vbu.SelectOption(label=bee.name, value=bee.id, description=bee.type_display)
+                for bee in drones.values()
+            ],
+            placeholder="Which princess would you like to breed?"
+        )))
+        drone_message = await payload.send("Which of your drones would you like to breed?", components=components)
+        try:
+            payload = await self.bot.wait_for("component_interaction", check=get_message_check(drone_message), timeout=60)
+        except asyncio.TimeoutError:
+            return await payload.send("I timed out waiting for you to say which drones you want to breed :c", wait=False)
+        drone = drones[payload.values[0]]
 
         # Breed the bee
+        await payload.defer()
         async with self.bot.database() as db:
             try:
                 new_bee = await utils.Bee.breed(db, princess, drone)
             except ValueError:
-                return await ctx.send("You can't breed anything other than a drone and a princess! :<", wait=False)
+                return await payload.send("You can't breed anything other than a drone and a princess! :<", wait=False)
 
         # Tell the user about their new queen
-        return await ctx.send(
+        return await payload.send(
             f"Your princess and drone got together and made a new {new_bee.type.value} queen, **{new_bee.display_name}**! :D",
             allowed_mentions=discord.AllowedMentions.none(),
             wait=False,
